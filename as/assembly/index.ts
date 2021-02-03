4a265ab9142b74a8e70bea55d53ea3f27bb9eae4
nstehr <nstehr@cisco.com>
2021-02-02 20:27:47 -0500

    playing with memory for passing types back and forth

--- as/assembly/index.ts
+++ as/assembly/index.ts
@@ -6,3 +6,32 @@ export function add(a: i32, b: i32): i32 {
   Console.log("FOOOO");
   return a + b;
 }
+
+// Allocate memory for a new byte array of
+// size `len` and return the offset into
+// the module's linear memory to the start
+// of the block.
+export function alloc(len: i32): usize {
+  // create a new AssemblyScript byte array
+  let buf = new Array<u8>(len);
+  let buf_ptr = memory.data(8);
+  // create a pointer to the byte array and
+  // return it
+  store<Array<u8>>(buf_ptr, buf);
+  return buf_ptr;
+}
+
+export function array_sum(buf_ptr: usize, len: i32): u8 {
+  let result: u8 = 0;
+  for(let i = 0; i < len; i++) {
+    result += load<u8>(buf_ptr + i) as u8;
+  }
+  return result as u8;
+}
+
+export function returnString(): ArrayBuffer {
+  let test = "This is a test of the emergency broadcast system";
+  let str = String.UTF8.encode(test, true)
+  return str;
+}
+
--- go/main.go
+++ go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	wasm "github.com/wasmerio/go-ext-wasm/wasmer"
 )
@@ -17,11 +18,60 @@ func main() {
 	wasiVersion := wasm.WasiGetVersion(module)
 
 	log.Println(wasiVersion)
+	if wasiVersion <= 0 {
+		return
+	}
 	importObject := wasm.NewDefaultWasiImportObjectForVersion(wasiVersion)
 	instance, _ := module.InstantiateWithImportObject(importObject)
-	log.Println(instance)
 	add := instance.Exports["add"]
-	log.Println(add)
 	result, _ := add(5, 37)
 	fmt.Println(result)
+
+	// test of allocating memory, writing an array and having the wasm function sum the elements in the array
+	tstArr := []uint8{2, 4, 6}
+	alloc := instance.Exports["alloc"]
+	// ptr is a pointer to a block of memory we've allocated on the wasm side
+	ptr, err := alloc(3)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// index into the block of memory we've reserved
+	memory := instance.Memory.Data()[int(ptr.ToI32()):]
+
+	// copy our array into that memory
+	for i := 0; i < len(tstArr); i++ {
+		memory[i] = tstArr[i]
+	}
+
+	sumArr := instance.Exports["array_sum"]
+	result, err = sumArr(ptr, 3)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// enjoy the results
+	log.Println(result)
+
+	// test of returning a string from WASM to go
+	returnStr := instance.Exports["returnString"]
+	ptr, err = returnStr()
+
+	i := 0
+	memory = instance.Memory.Data()[int(ptr.ToI32()):]
+	var output strings.Builder
+
+	for {
+		// on the wasm side, we null terminated the string, so we can rely on this C style :)
+		if memory[i] == 0 {
+			break
+		}
+
+		output.WriteByte(memory[i])
+		i++
+	}
+
+	fmt.Println(output.String())
 }
